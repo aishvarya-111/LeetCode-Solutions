3583237bae5aab520ae4407c9b3a9bbac5bdc954
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-10 09:22:07 +0530

    Update 532-k-diff-pairs-in-an-array.py

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        nums.sort()
+        nums.sort() #sorting the array
         c=0
         d=[]
         for i in range(len(nums)):
-            if nums[i]+k in nums[i+1:] and nums[i] not in d:
+            if nums[i]+k in nums[i+1:] and nums[i] not in d: #avoiding duplicates 
                 d.append(nums[i])
                 c+=1
         return c
