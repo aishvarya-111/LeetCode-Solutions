55759eb19fa49090ecf90e80c05c92ac72e8cc7a
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-12 22:55:13 +0530

    Added solution - LeetHub

--- Maximum number of events that can be attended - GFG/maximum-number-of-events-that-can-be-attended.py
+++ Maximum number of events that can be attended - GFG/maximum-number-of-events-that-can-be-attended.py
@@ -3,26 +3,22 @@ import heapq
 class Solution:
     def maxEvents(self, start, end, N):
         # code here 
-        events = []
-        for s1, e1 in zip(start, end):
-            events.append([s1, e1])
-        events.sort(key=lambda x:(x[0],x[1]))
-        idx=0
-        n=len(events)
-        heap=[]
-        res=0
-        d=events[idx][0]
-        while heap or idx<n:
-            if not heap:
-                d=events[idx][0]
-            while idx<n and events[idx][0]<=d:
-                heapq.heappush(heap, events[idx][1])
-                idx+=1
-            heapq.heappop(heap)
-            res+=1
-            d+=1
-            while heap and heap[0]<d:
-                heapq.heappop(heap)
+        A=[] 
+        for i in range(N):
+            A.append([start[i],end[i]])
+        
+        A.sort(reverse=True)
+        h = []
+        res = d = 0
+        while A or h:
+            if not h: d = A[-1][0]
+            while A and A[-1][0] <= d:
+                heapq.heappush(h, A.pop()[1])
+            heapq.heappop(h)
+            res += 1
+            d += 1
+            while h and h[0] < d:
+                heapq.heappop(h)
         return res
 #{ 
 #  Driver Code Starts
