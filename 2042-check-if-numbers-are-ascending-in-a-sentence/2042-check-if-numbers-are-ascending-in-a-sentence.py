c35ff66fd097adacdd16c82bf832030caf894e14
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-13 19:13:40 +0530

    Time: 45 ms (42.45%), Space: 13.9 MB (91.18%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.py
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.py
@@ -1,10 +1,7 @@
 class Solution:
     def areNumbersAscending(self, s: str) -> bool:
-        l = list(s.split())
-        pn = -1
-        for i in l:
-            if i.isnumeric() and int(i)>pn:
-                pn = int(i)
-            elif i.isnumeric() and int(i)<=pn:
-                return False
-        return True
+        s = s.split()
+        l = [int(i) for i in s if i.isnumeric()]
+        if len(l)>len(set(l)):
+            return False
+        return sorted(l)==l
