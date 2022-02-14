0b6761b431f5a48ca81330e463e9aaa70b2865aa
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-14 22:36:15 +0530

    Added solution - LeetHub

---
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+    def findTwoElement( self,arr, n): 
+        # code here
+        arr.sort()
+        l = [*range(1,n+1)]
+        r = sum(arr) - sum(set(arr))
+        sn = (n*(n+1))//2
+        a = sn-sum(set(arr))
+        return [r,a]
+            
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+
+    tc=int(input())
+    while tc > 0:
+        n=int(input())
+        arr=list(map(int, input().strip().split()))
+        ob = Solution()
+        ans=ob.findTwoElement(arr, n)
+        print(str(ans[0])+" "+str(ans[1]))
+        tc=tc-1
+# } Driver Code Ends
