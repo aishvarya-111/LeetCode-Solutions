ed5eb443ca04312e0b851df7a5990f1c3c831ff6
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-16 00:10:36 +0530

    Time: 63 ms (11.04%), Space: 13.8 MB (93.15%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -1,24 +1,8 @@
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
-        
-        for i in word1:
-            if i in word2:
-                word1 = word1.replace(i,"",1)
-                word2 = word2.replace(i,"",1)
-        
-        d1 = {}
-        d2 = {}
-        for i in word1:
-            if i in d1:
-                d1[i]+=1
-            else:
-                d1[i]=1
-        for i in word2:
-            if i in d2:
-                d2[i]+=1
-            else:
-                d2[i]=1
-        
+        from collections import Counter
+        d1 = Counter(word1)
+        d2 = Counter(word2)
         for k,v in d1.items():
             if k in d2 and abs(v-d2[k])>3:
                 return False
