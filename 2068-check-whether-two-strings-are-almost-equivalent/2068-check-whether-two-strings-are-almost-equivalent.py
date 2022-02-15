1156cc612514edbbf925fd5b4a3b4be02efe2e09
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-16 00:13:28 +0530

    Time: 68 ms (5.93%), Space: 13.8 MB (93.15%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -4,12 +4,10 @@ class Solution:
         d1 = Counter(word1)
         d2 = Counter(word2)
         for k,v in d1.items():
-            if k in d2 and abs(v-d2[k])>3:
-                return False
-            if k not in d2 and v>3:
+            if abs(v-d2[k])>3:
                 return False
         for k,v in d2.items():
-            if k not in d1 and v>3:
+            if abs(v-d1[k])>3:
                 return False
         return True
                 
