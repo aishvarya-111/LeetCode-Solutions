d92f781d4610fcb3fb730349c89a7587c490bb57
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-16 15:45:16 +0530

    Time: 142 ms (53.11%), Space: 14.2 MB (82.30%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -3,8 +3,6 @@ class Solution:
         if len(nums)==1:
             return 0
         nums.sort(reverse=True)
-        if len(nums)==k:
-            return abs(nums[0]-nums[-1])
         mi = sum(nums)
         for i in range(len(nums)-k+1):
             m = nums[i] - nums[i+k-1]
