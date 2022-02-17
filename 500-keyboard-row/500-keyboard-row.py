bd27fd59512679012dd32e595cff2368f02b7938
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-17 17:26:24 +0530

    Time: 49 ms (30.47%), Space: 14 MB (71.61%) - LeetHub

--- 500-keyboard-row/500-keyboard-row.py
+++ 500-keyboard-row/500-keyboard-row.py
@@ -1,16 +1,13 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        a = "qwertyuiopQWERTYUIOP"
-        b = "asdfghjklASDFGHJKL"
-        c = "zxcvbnmZXCVBNM"
         res = []
         for i in words:
             aa,bb,cc,n = 0,0,0,len(i)
             for j in i:
                 if j in i:
-                    if j in a:
+                    if j in "qwertyuiopQWERTYUIOP":
                         aa+=1
-                    elif j in b:
+                    elif j in "asdfghjklASDFGHJKL":
                         bb+=1
                     else:
                         cc+=1
