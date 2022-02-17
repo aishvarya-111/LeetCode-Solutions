fd7ba02e0b1c4c5fec411e25cf21465aeda672ce
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-17 20:47:55 +0530

    Time: 36 ms (60.32%), Space: 13.9 MB (70.36%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        res=""
+        for i in range(len(s)):
+            if i%2==0:
+                res+=s[i]
+            else:
+                res+=chr(ord(s[i-1])+int(s[i]))
+        return res
+                
