ba22eb05268a0e263aa561390793fbb39093e314
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-17 21:19:53 +0530

    Time: 40 ms (52.88%), Space: 14 MB (60.73%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,6 +1,4 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        a = []
-        for i in sentence:
-            a.append(ord(i)-96)
+        a = [ord(i)-96 for i in sentence]
         return sorted(set(a)) == [*range(1,27)]
