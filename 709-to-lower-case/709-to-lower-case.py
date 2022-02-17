d6d837834e0af2a6cda7384b516b2e49d24cb568
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-17 17:19:34 +0530

    Time: 35 ms (61.29%), Space: 13.9 MB (67.72%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.py
+++ 709-to-lower-case/709-to-lower-case.py
@@ -1,11 +1,3 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        ans = ""
-        
-        for x in s:
-            if ord(x) >= 65 and ord(x) <= 90:
-                ans += chr(ord(x) + 32)
-            else:
-                ans += x      
-        return ans
-    
+        return s.lower()
