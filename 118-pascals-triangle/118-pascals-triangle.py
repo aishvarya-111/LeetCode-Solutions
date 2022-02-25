c085bd50e5a128b4de04e081091752d2605468c2
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-02-25 16:46:00 +0530

    Time: 44 ms (48.69%), Space: 13.9 MB (87.21%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -3,12 +3,11 @@ class Solution:
         if numRows == 1:
             return [[1]]
         res = [[1],[1,1]]
-        
         for i in range(numRows-2):
             l = res[-1]
             a = [1]
             for i in range(len(l)-1):
                 a.append(l[i]+l[i+1])
-            a.append(1)
+            a = a+[1]
             res.append(a)
         return res
