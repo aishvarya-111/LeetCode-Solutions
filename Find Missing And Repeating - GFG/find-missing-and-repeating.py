aa18cc43d8d51574bc64888417db466037374e92
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-04-15 14:00:19 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.py
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.py
@@ -3,11 +3,17 @@
 class Solution:
     def findTwoElement( self,arr, n): 
         # code here
-        arr.sort()
-        r = sum(arr) - sum(set(arr))
-        sn = (n*(n+1))//2
-        a = sn-sum(set(arr))
-        return [r,a]
+        s1 = (n*(n+1))//2
+        s2 = (n*(n+1)*(2*n+1))//6
+        
+        for i in arr:
+            s1-=i
+            s2-=(i*i)
+        d = s2//s1
+        
+        m = (s1+d)//2
+        r = m-s1
+        return [r,m]
             
 
 #{ 
