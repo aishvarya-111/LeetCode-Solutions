85cffbae681e3c6e56b27c573ced16a9fa806804
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-04-15 12:56:36 +0530

    Time: 39 ms (73.54%), Space: 13.8 MB (67.40%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,9 +1,9 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColors(self, n: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        z,o,t=0,0,0
+        """z,o,t=0,0,0
         for i in nums:
             if i==0:
                 z+=1
@@ -17,3 +17,17 @@ class Solution:
             nums[i]=1
         for i in range(z+o,z+o+t):
             nums[i]=2
+        """
+        l,m,h=0,0,len(n)-1
+        while(m<=h):
+            if n[m]==0:
+                n[l],n[m]=n[m],n[l]
+                m+=1
+                l+=1
+            elif n[m]==1:
+                m+=1
+            elif n[m]==2:
+                n[m],n[h]=n[h],n[m]
+                h-=1
+        
+            
