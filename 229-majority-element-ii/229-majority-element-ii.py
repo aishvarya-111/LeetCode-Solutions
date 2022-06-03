db8f7eeafa3fda2332b2c909fa8800fbcc010c1f
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-06-03 21:29:57 +0530

    Time: 278 ms (5.05%), Space: 15.3 MB (72.11%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,15 +1,35 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        if len(nums)==1:
+            return nums
         n = len(nums)
-        d = {}
-        for i in nums:
-            if i in d:
-                d[i]+=1
+        fs,ss = 0,0
+        fm,sm = nums[0],nums[1] 
+        for i in range(n):
+            if fm==nums[i]:
+                fs+=1 
+            elif sm== nums[i]:
+                ss+=1 
+            elif fs==0:
+                fm=nums[i]
+                fs+=1 
+            elif ss==0:
+                sm = nums[i]
+                ss+=1
             else:
-                d[i]=1 
+                fs-=1 
+                ss-=1 
+                
+        fs = 0
+        ss = 0
+        for i in range(n):
+            if nums[i]==fm:
+                fs+=1 
+            elif nums[i]==sm:
+                ss+=1
         ans = []
-      
-        for k in d:
-            if d[k]>(n//3):
-                ans.append(k)
+        if fs > n//3:
+            ans.append(fm)
+        if ss > n//3:
+            ans.append(sm)
         return ans
