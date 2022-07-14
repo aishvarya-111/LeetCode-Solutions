e8c5ca1fd489b07542638d4083a3d31e97670390
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-07-14 20:23:04 +0530

    Time: 1566 ms (8.57%), Space: 27.9 MB (78.04%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -3,8 +3,8 @@ class Solution:
         s = 0
         m = nums[0]
         for i in range(len(nums)):
-            if s<0:
-                s=0
             s+=nums[i]
             m = max(m,s)
+            if s<0:
+                s = 0
         return m
