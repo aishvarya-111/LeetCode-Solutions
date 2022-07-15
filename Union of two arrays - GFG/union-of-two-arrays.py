9acf7427f16a1dd5b38ce99aeb9ce0669cdc98a0
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-07-15 19:49:55 +0530

    Added solution - LeetHub

--- Union of two arrays - GFG/union-of-two-arrays.py
+++ Union of two arrays - GFG/union-of-two-arrays.py
@@ -3,19 +3,16 @@
 class Solution:    
     #Function to return the count of number of elements in union of two arrays.
     def doUnion(self,a,n,b,m):
-        x = [0]*(max(a)+1)
-        y = [0]*(max(b)+1) 
+        d = {}
         c=0
-        for i in range(n):
-            x[a[i]]+=1 
-            
-        for i in range(m):
-            y[b[i]]+=1
-        
-        for i in range(max(max(a),max(b))+1):
-            if (i<len(x) and x[i]) or (i<len(y) and y[i]):
-                
+        for i in a:
+            if i not in d:
+                d[i]=1
                 c+=1 
+        for i in b:
+            if i not in d:
+                d[i]=1 
+                c+=1
         return c
         #code here
 
