b8f62f18bb1e7e69346fb6cc19df47f58292443a
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-07-15 10:58:21 +0530

    Time: 199 ms (12.21%), Space: 14.8 MB (38.24%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -4,18 +4,7 @@ class Solution:
         n = len(mat[0])
         if m*n!=(r*c):
             return mat
-        a = []
-        for i in range(m):
-            for j in range(n):
-                a.append(mat[i][j])
-        k = 0
-        print(a)
-        ans=[]
-        for i in range(r):
-            p = []
-            for j in range(c):
-                if k<len(a):
-                    p.append(a[k])
-                    k+=1
-            ans.append(p)
+        ans = [[0]*c for i in range(r)] 
+        for i in range(m*n):
+            ans[i//c][i%c] = mat[i//n][i%n]
         return ans
