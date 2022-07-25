d206943c931f78018c4d66ed5d05721fe6c58af5
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-07-26 01:26:45 +0530

    Time: 2105 ms (21.26%), Space: 74.9 MB (5.17%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,17 +1,26 @@
-class Solution(object):
-    def findSmallestSetOfVertices(self, n, edges):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :rtype: List[int]
-        """
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(set)
         
-        al = [[] for _ in range(n)]
+        visited = set()
+        result = set()
         
-        for i in edges:
-            al[i[1]].append(i[0])
-        ans = []
-        for i in range(len(al)):
-            if len(al[i]) ==0:
-                ans.append(i)
-        return ans
+        for src,dest in edges:
+            graph[src].add(dest)
+            
+        
+        def dfs(src):
+            visited.add(src)
+            for dest in graph[src]:
+                if dest not in visited:
+                    dfs(dest)
+                elif dest in result:
+                    result.remove(dest)
+                    
+        
+        for i in range(n):
+            if i not in visited:
+                result.add(i)
+                dfs(i)
+                
+        return result
