2e762543ffdc631e9d37538ef41fd39cef2707eb
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-07-25 14:27:05 +0530

    Time: 788 ms (20.58%), Space: 15.9 MB (51.50%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,4 +1,16 @@
 from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return list(combinations([*range(1,n+1)],k))
+        ans = []
+        def com(ind,a):
+            if len(a)==k:
+                ans.append(a.copy())
+                return
+            
+            for i in range(ind,n+1):
+                a.append(i)
+                com(i+1,a)
+                a.pop()
+        
+        com(1,[])
+        return ans
