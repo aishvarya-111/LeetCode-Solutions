c810500fe274130798022311f3acce1ac41c54ed
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-07-28 09:02:31 +0530

    Time: 74 ms (54.74%), Space: 14.6 MB (34.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,13 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         a = [0]*26
-        b = [0]*26 
-        
+    
         for i in s:
             idx = ord(i)-ord('a')
             a[idx]+=1
         for i in t:
             idx = ord(i)-ord('a')
-            b[idx]+=1
-        
-        return a==b
+            a[idx]-=1
+        for i in a:
+            if i!=0:
+                return False
+        return True
