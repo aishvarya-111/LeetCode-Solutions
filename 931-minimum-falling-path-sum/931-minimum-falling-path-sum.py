6c74db76544c9dc7a0de7169ec65cf6291ab024d
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-07-31 07:16:04 +0530

    Time: 173 ms (79.64%), Space: 14.9 MB (26.19%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,13 +1,12 @@
 class Solution:
     def minFallingPathSum(self, mat: List[List[int]]) -> int:
         n = len(mat)
-        m = len(mat[0])
         for i in range(1,n):
             for j in range(n):
                 temp = mat[i-1][j]
                 if j>0:
                     temp = min(temp,mat[i-1][j-1])
-                if j<m-1:
+                if j<n-1:
                     temp = min(temp,mat[i-1][j+1])
                 mat[i][j]+=temp 
         
