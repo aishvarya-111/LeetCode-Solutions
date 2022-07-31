12ae100f9676d0fda811b6b2a5c2bfe2e8a1f9f9
Aishvarya C <71811151+aishvarya-111@users.noreply.github.com>
2022-07-31 23:03:51 +0530

    Time: 118 ms (71.24%), Space: 16.6 MB (33.99%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -3,14 +3,20 @@ class Solution:
         if not nums:
             return 0
         dic = collections.defaultdict(int)
-        minimum = float('inf')
-        maximum = -float('inf')
         for n in nums:
             dic[n] += 1
-            maximum = max(maximum, n)
-            minimum = min(minimum, n)
+        
+        nums = sorted(list(set(nums)))
         prev = 0
-        curr = 0
-        for i in range(minimum, maximum+1):
-            prev, curr = curr, max(prev + dic[i]*i, curr)
-        return curr
+        prev2 = 0
+        for i in range(len(nums)):
+            curr = nums[i]*dic[nums[i]]
+            if i>0 and nums[i]==nums[i-1]+1:
+                temp = prev
+                prev= max(curr+prev2,prev)
+                prev2 = temp
+            else:
+                temp = prev
+                prev = curr+prev
+                prev2 = temp
+        return prev
